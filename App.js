137e5f78ca6141fdff5250379b01faf6ac3e8c54
philanth <125470319+philanth@users.noreply.github.com>
2024-02-15 16:21:33 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RegistrationComponent from './Components/Assets/Registration'; // Updated the import identifier
+import './Registration.css';
+
+function App() {
+  return (
+    <div>
+      <RegistrationComponent/>
+    </div>
+  );
+}
+
+export default App;
